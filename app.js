f8fb923270b3477d125cfd09a7740f1000f0cb7e
Rohit <vr.rohit03@gmail.com>
2021-06-01 19:00:51 +0530

    First soln

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const data = require("./prize.json");
+const _ = require("lodash");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h1>Working!</h1>");
+app.use(express.urlencoded({extended: true}));
+
+function searchByFirstName(data, fname) {
+  var foundData = [];
+  data.prizes.forEach((eachprize) => {
+    eachprize.laureates.forEach((eachlaureates) => {
+      if(_.lowerCase(eachlaureates.firstname) === fname) {
+        foundData.push(eachlaureates);
+      }
+    })
+  });
+  return foundData;
+}
+
+app.get('/searchbyfirstname/:fname', (req, res) => {
+
+  const fname = _.lowerCase(req.params.fname);
+
+  const showData = searchByFirstName(data, fname);
+  res.send(showData);
+
 });
 
 app.listen(PORT, () => {
